c936ee25cd262bc1a665333a64621093f4f1d096
Maruf Khan <maruf1609@gmail.com>
2021-08-11 19:14:20 +0600

    listview implemented instead of scroll view

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -3,62 +3,66 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-  List<Transaction> transaction = [];
+  List<Transaction> transactions = [];
 
-  TransactionList(this.transaction);
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: transaction.map((e) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                child: Text(
-                  '\$${e.amount}',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                    style: BorderStyle.solid,
-                  ),
-                ),
-                padding: EdgeInsets.all(10),
-              ),
-              Column(
-                children: [
-                  Container(
-                    child: Text(
-                      e.title,
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+    return Container(
+      height: 258,
+      child: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            child: Row(
+              children: [
+                Container(
+                  child: Text(
+                    '\$${transactions[index].amount}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
                     ),
                   ),
-                  Container(
-                    child: Text(
-                      DateFormat.yMd().format(e.date),
-                      style: TextStyle(
-                        color: Colors.grey,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                      style: BorderStyle.solid,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                ),
+                Column(
+                  children: [
+                    Container(
+                      child: Text(
+                        transactions[index].title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  )
-                ],
-                crossAxisAlignment: CrossAxisAlignment.start,
-              )
-            ],
-          ),
-        );
-      }).toList(),
+                    Container(
+                      child: Text(
+                        DateFormat.yMd().format(transactions[index].date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
